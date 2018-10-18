21fc7d7c81462351603ad827c1b8a4f0b754d8cd
FranziGrieser <franzi.grieser@gmail.com>
2018-10-18 14:51:49 +0200

    email setup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
+  post 'simple_pages/thank_you'
+
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :orders, only: [:index, :show, :create, :destroy]
