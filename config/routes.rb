cfdbd20f8022b7120130dfbb3ea7fb2c06878181
FranziGrieser <franzi.grieser@gmail.com>
2018-10-19 14:28:55 +0200

    authentication  devise login style forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
+  resources :users
   resources :products
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
+  get 'simple_pages/thank_you'
   post 'simple_pages/thank_you'
-
-  root 'products#index'
+  root 'simple_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :orders, only: [:index, :show, :create, :destroy]
 end
