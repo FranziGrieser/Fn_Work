0a055a05557c2ef0fb4ab8a3c92c565b8cfae6be
FranziGrieser <franzi.grieser@gmail.com>
2018-11-08 18:05:51 +0100

    6.3 test users controller functions

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,24 +2,24 @@ require 'rails_helper'
 
 describe UsersController, type: :controller do
   let(:user) {User.create!(email: 'user@testmail.com', password: 'testers')}
-  let(:user) {User.create!(email: 'user2@testmail.com', password: 'testers2')}
+  let(:user2) {User.create!(email: 'user2@testmail.com', password: 'testers2')}
 
   describe 'GET #show' do
     context 'when a user is logged in' do
 
       before do
-        sign_in @user
+        sign_in user
       end
 
       it 'loads correct user details' do
-        get :show, params: {id: @user.id}
+        get :show, params: {id: user.id}
         expect(response).to be_ok
         expect(assigns(:user)).to eq user
       end
 
       it 'second user cannot access other users page' do
-        get :show, params: {id: @user2.id}
-        expect(response).to have_http_status(401)
+        get :show, params: {id: user2.id}
+        expect(response).to have_http_status(302)
         expect(response).to redirect_to(root_path)
       end
 
@@ -27,7 +27,7 @@ describe UsersController, type: :controller do
 
     context 'when a user is not logged in' do
       it 'redirects to login' do
-        get :show, params: {id: @user.id}
+        get :show, params: {id: user.id}
         expect(response).to redirect_to(new_user_session_path)
       end
     end
