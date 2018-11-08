1dbc0f4f31708a145f4c4b27eb11c2385b5ba83a
FranziGrieser <franzi.grieser@gmail.com>
2018-11-08 20:58:15 +0100

    6.3 test comments_controller

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe CommentsController, type: :controller do
+  let(:user) {User.create!(email: 'user@testmail.com', password: 'testers')}
+  let(:product) { Product.create!(name: "bike") }
+
+  context 'when user is logged in' do
+
+    before do
+      sign_in user
+    end
+
+    it 'can create comments' do
+      post :create, params: { product_id: product.id, comment: { body: "fantastic", rating: 5 } }
+      expect(response).to have_http_status(302)
+    end
+  end
+end
