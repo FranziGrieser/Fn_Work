f84880f8a2c22490484a40f4165843bf433ba1d5
FranziGrieser <franzi.grieser@gmail.com>
2018-11-09 15:16:24 +0100

    6.4 overwrite factories in tests

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 describe OrdersController, type: :controller do
-  let(:user) {User.create!(email: 'user@testmail.com', password: 'testers')}
+#  let(:user) {User.create!(email: 'user@testmail.com', password: 'testers')}
+
+  before do
+    @user = FactoryBot.build(:user)
+  end
 
   context 'unauthenticated user' do
     it "redirects to login" do
-      get :show, params: {id: user.id}
+      get :index
       expect(response).to redirect_to(new_user_session_path)
     end
   end
