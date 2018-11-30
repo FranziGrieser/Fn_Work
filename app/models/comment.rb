8f829158ded7282af5afc1600de45b096aed83f8
FranziGrieser <franzi.grieser@gmail.com>
2018-11-30 21:43:46 +0100

    DRY code

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
-  validates :body, presence: true
-  validates :user, presence: true
-  validates :product, presence: true
+  validates :body, :user, :product, presence: true
   validates :rating, numericality: { only_integer: true}
   after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
   scope :rating_desc, -> { order(rating: :desc) }
