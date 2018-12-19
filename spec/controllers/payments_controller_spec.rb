8917f94bcdfc29762dda4b67c1cf144deef356aa
FranziGrieser <franzi.grieser@gmail.com>
2018-12-19 06:20:20 +0100

    stripe_mock and payments tests

--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -1,17 +1,6 @@
 require 'rails_helper'
-require 'stripe_mock'
 
 RSpec.describe PaymentsController, type: :controller do
 
-  it "mocks a declined card error" do
-  # Prepares an error for the next create charge request
-  StripeMock.prepare_card_error(:card_declined)
-
-  expect { Stripe::Charge.create(amount: 1, currency: 'usd') }.to raise_error {|e|
-    expect(e).to be_a Stripe::CardError
-    expect(e.http_status).to eq(402)
-    expect(e.code).to eq('card_declined')
-  }
-end
 
 end
