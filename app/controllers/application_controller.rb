e1966538de0039404990e3415c03ea5ad74f4ee6
FranziGrieser <franzi.grieser@gmail.com>
2019-01-17 19:36:24 +0100

    def current_order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: exception
+  helper_method :current_order
+
+  def current_order
+    if !session[:order_id].nil?
+      Order.find(session[:order_id])
+    else
+      Order.new
+    end
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, alert: exception.message
   end
+
 end
