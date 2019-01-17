506e64d3462046614e32688b73a0e9ac4d378b5e
FranziGrieser <franzi.grieser@gmail.com>
2019-01-17 19:23:52 +0100

    associations, callbacks, validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
-  has_many :orders
-  has_many :comments
+  has_many :order_items
+  has_many :comments, dependent: :destroy
+
   validates :name, presence: true
   validates :image_url, presence: true
 
