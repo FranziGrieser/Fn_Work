eb560339db3bb966333ab96ed27ec0454805fcdb
FranziGrieser <franzi.grieser@gmail.com>
2019-01-17 21:03:37 +0100

    order_items

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :order_items, only [:create, :update, :destroy]
+  resources :order_items, only: [:create, :update, :destroy]
   get 'carts/show'
 
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }, controllers: { registrations: "user_registrations"}
