2ebf61d39efc23dbf92aefeb8a23228b72535126
FranziGrieser <franzi.grieser@gmail.com>
2019-01-23 14:02:30 +0100

    cart issue

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  skip_before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
     if current_user.admin?
@@ -9,30 +9,15 @@ class OrdersController < ApplicationController
     end  end
 
   def show
-    @order = Order.find(params[:id]).to_json(:include => [{ :user => { :only => :email }}])
+    @order = Order.find(params[:id])
   end
 
   def new
   end
 
   def create
-    @order = Order.create(order_params)
-    respond_with @order
-    session[:order_id] = @order.id
   end
 
   def destroy
-    respond_with Order.destroy(params[:id])
-    session[:order_id] = nil
-  end
-
-  protected
-    def json_request?
-      request.format.json?
-    end
-
-  private
-  def order_params
-    params.require(:order).permit(:total, :user_id)
   end
 end
